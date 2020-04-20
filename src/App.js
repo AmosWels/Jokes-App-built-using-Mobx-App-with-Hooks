276a3c4cfa70d3b1e91b5664afd6a29dab7d2bb8
Amos Welike <amoswelike@gmail.com>
2020-04-20 23:38:47 +0300

    add new files for dsplaying api data

--- src/App.js
+++ src/App.js
@@ -1,77 +1,77 @@
-import React from "react";
-import { useLocalStore, useObserver } from "mobx-react";
+// import React from "react";
+// import { useLocalStore, useObserver } from "mobx-react";
 
-const StoreContext = React.createContext();
+// const StoreContext = React.createContext();
 
-const StoreProvider = ({ children }) => {
-  const store = useLocalStore(() => ({
-    bugs: ["Centipede"],
-    addBug: bug => {
-      store.bugs.push(bug);
-    },
-    get bugsCount() {
-      return store.bugs.length;
-    }
-  }));
+// const StoreProvider = ({ children }) => {
+//   const store = useLocalStore(() => ({
+//     bugs: ["Centipede"],
+//     addBug: bug => {
+//       store.bugs.push(bug);
+//     },
+//     get bugsCount() {
+//       return store.bugs.length;
+//     }
+//   }));
 
-  return (
-    <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
-  );
-};
+//   return (
+//     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
+//   );
+// };
 
-const BugsHeader = () => {
-  const store = React.useContext(StoreContext);
-  // return useObserver(() => <h1>{store.bugsCount} Bugs!</h1>);
-  return useObserver(()=>
-    <h1>{store.bugsCount} Bugs!</h1>
-  )
+// const BugsHeader = () => {
+//   const store = React.useContext(StoreContext);
+//   // return useObserver(() => <h1>{store.bugsCount} Bugs!</h1>);
+//   return useObserver(()=>
+//     <h1>{store.bugsCount} Bugs!</h1>
+//   )
   
-};
+// };
 
-const BugsList = () => {
-  const store = React.useContext(StoreContext);
+// const BugsList = () => {
+//   const store = React.useContext(StoreContext);
 
-  return useObserver(() => (
-    <ul>
-      {store.bugs.map(bug => (
-        <li key={bug}>{bug}</li>
-      ))}
-    </ul>
-  ));
-};
+//   return useObserver(() => (
+//     <ul>
+//       {store.bugs.map(bug => (
+//         <li key={bug}>{bug}</li>
+//       ))}
+//     </ul>
+//   ));
+// };
 
-const BugsForm = () => {
-  const store = React.useContext(StoreContext);
-  const [bug, setBug] = React.useState("");
+// const BugsForm = () => {
+//   const store = React.useContext(StoreContext);
+//   const [bug, setBug] = React.useState("");
 
-  return (
-    <form
-      onSubmit={e => {
-        store.addBug(bug);
-        setBug("");
-        e.preventDefault();
-      }}
-    >
-      <input
-        type="text"
-        value={bug}
-        onChange={e => {
-          setBug(e.target.value);
-        }}
-      />
-      <button type="submit">Add</button>
-    </form>
-  );
-};
+//   return (
+//     <form
+//       onSubmit={e => {
+//         store.addBug(bug);
+//         setBug("");
+//         e.preventDefault();
+//       }}
+//     >
+//       <input
+//         type="text"
+//         value={bug}
+//         onChange={e => {
+//           setBug(e.target.value);
+//         }}
+//       />
+//       <button type="submit">Add</button>
+//     </form>
+//   );
+// };
 
-export default function App() {
-  return (
-    <StoreProvider>
-      <main>
-        <BugsHeader />
-        <BugsList />
-        <BugsForm />
-      </main>
-    </StoreProvider>
-  );
-}
+// export default function App() {
+//   return (
+//     <StoreProvider>
+//       <main>
+//         <BugsHeader />
+//         <BugsList />
+//         <BugsForm />
+//       </main>
+//     </StoreProvider>
+//   );
+// }
